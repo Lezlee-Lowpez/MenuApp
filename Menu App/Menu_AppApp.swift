617062a24a325da0d773a454e6c9541087da7ef9
Lesley Lopez <lopez.lesley96@yahoo.com>
2024-06-04 12:57:12 -0700

    Uploading Menu App

---
+++ Menu App/Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu App
+//
+//  Created by Lesley Lopez on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
